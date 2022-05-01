29314dc91c2ac2abb2633cbb2ebc94f158c72d99
dacracot <dacracot@yahoo.com>
2022-05-01 07:24:45 -0700

    zip last month weekly

--- src/bash/weekly.sh
+++ src/bash/weekly.sh
@@ -3,6 +3,7 @@ SECONDS=0
 # ---------------------------------------------------
 YEAR=`date '+%Y'`
 MONTH=`date '+%m'`
+LASTMONTH=`date -d $(date +%Y)-$(( $(date +%m) - 1 ))-15 +%m`
 DAY=`date '+%d'`
 WEEK=`date '+%U'`
 LAT=37.713
@@ -12,10 +13,10 @@ LON=-121.440
 python3 /home/tweet/BirdNET-Analyzer/species.py --o /home/tweet/samples/species_list.txt --lat ${LAT} --lon ${LON} --week ${WEEK}
 find /home/tweet/samples -mtime +14 -name "*.gz" -delete
 find /home/tweet/samples -empty -type d -delete
-gzip /home/tweet/${YEAR}-${MONTH}-${DAY}-afterDark.err
-gzip /home/tweet/${YEAR}-${MONTH}-${DAY}-afterDark.out
-gzip /home/tweet/${YEAR}-${MONTH}-${DAY}-beforeDawn.err
-gzip /home/tweet/${YEAR}-${MONTH}-${DAY}-beforeDawn.out
+gzip /home/tweet/${YEAR}-${LASTMONTH}-*-afterDark.err
+gzip /home/tweet/${YEAR}-${LASTMONTH}-*-afterDark.out
+gzip /home/tweet/${YEAR}-${LASTMONTH}-*-beforeDawn.err
+gzip /home/tweet/${YEAR}-${LASTMONTH}-*-beforeDawn.out
 df -h
 DURATION=$SECONDS
 echo "$(($DURATION / 60)) minutes and $(($DURATION % 60)) seconds elapsed."
