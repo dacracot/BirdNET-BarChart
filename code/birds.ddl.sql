7c17c83e0c78cbe3381db938f2db70b22e9b6f44
David Craig Cothren <dacracot@yahoo.com>
2023-05-10 15:12:10 -0700

    matched old code with implemented code

--- code/birds.ddl.sql
+++ code/birds.ddl.sql
@@ -1,5 +1,4 @@
-CREATE TABLE clock (hour text);
-
+/*------------------------------------------------------------------------*/
 CREATE TABLE heardRaw (
 	startSecond FLOAT NOT NULL,
 	endSecond FLOAT NOT NULL,
@@ -8,15 +7,38 @@ CREATE TABLE heardRaw (
 	confidence FLOAT NOT NULL,
 	minuteOfDay DATETIME NOT NULL
 );
-
+/*------------------------------------------------------------------------*/
 CREATE TABLE heardSumByHour (
 	hourOfDay DATETIME NOT NULL,
 	commonName TEXT NOT NULL,
 	howMany INTEGER NOT NULL
 );
-
+/*------------------------------------------------------------------------*/
 CREATE VIEW distinctSeen AS
 	SELECT
 		DISTINCT commonName
 	FROM
-		heardRaw
+		heardRaw;
+/*------------------------------------------------------------------------*/
+CREATE TABLE clock (
+	hour TEXT NOT NULL
+);
+/*-------------------------------------*/
+INSERT INTO clock VALUES
+	('05:00'),
+	('06:00'),
+	('07:00'),
+	('08:00'),
+	('09:00'),
+	('10:00'),
+	('11:00'),
+	('12:00'),
+	('13:00'),
+	('14:00'),
+	('15:00'),
+	('16:00'),
+	('17:00'),
+	('18:00'),
+	('19:00'),
+	('20:00');
+/*------------------------------------------------------------------------*/
