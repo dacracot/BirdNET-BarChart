47a1ea561aa429c41601f8bb9b07de291a325a35
David Craig Cothren <dacracot@yahoo.com>
2025-02-17 08:03:25 -0800

    working table sort

--- chart/chart.js
+++ chart/chart.js
@@ -135,28 +135,26 @@ $(function(){
 	bars = tab.querySelectorAll("rect");
 	bars.forEach(b => {b.style = "fill:rgb("+random.nextRandom()+","+random.nextRandom()+","+random.nextRandom()+")";});
 	// 	------------------------------
-	// Code from https://jsfiddle.net/zscQy/
+	// table sorting
 	function sortTable(table, col, reverse) {
-		var tb = table.tBodies[0], // use `<tbody>` to ignore `<thead>` and `<tfoot>` rows
-			tr = Array.prototype.slice.call(tb.rows, 0), // put rows into array
-			i;
+		// use `<tbody>` to ignore `<thead>` and `<tfoot>` rows
+		var tb = table.tBodies[0],
+			// collect the rows into an array
+			tr = Array.prototype.slice.call(tb.rows, 0);
 		reverse = -((+reverse) || -1);
 		tr = tr.sort(function (a, b) {
-console.log("===============================");
-		testStringOrNumber = Number(a.cells[col].textContent.trim());
-console.log("testStringOrNumber = "+testStringOrNumber);
-		if (Number.isNaN(testStringOrNumber)) {
-console.log("string");
-			return reverse * (a.cells[col].textContent.trim().localeCompare(b.cells[col].textContent.trim()));
-			}
-		else {
-console.log("number");
-			return reverse * (Number(a.cells[col].textContent.trim()) - Number(b.cells[col].textContent.trim()));
-			}
-		});
-console.log("-------------------------------");
-		for(i = 0; i < tr.length; ++i) tb.appendChild(tr[i]); // append each row in order
+			testStringOrNumber = Number(a.cells[col].textContent.trim());
+			if (Number.isNaN(testStringOrNumber)) {
+				return reverse * (a.cells[col].textContent.trim().localeCompare(b.cells[col].textContent.trim()));
+				}
+			else {
+				return reverse * (Number(a.cells[col].textContent.trim()) - Number(b.cells[col].textContent.trim()));
+				}
+			});
+		// put the sorted rows back
+		for(var i = 0; i < tr.length; ++i) tb.appendChild(tr[i]);
 		}
+	// 	------------------------------
 	function makeSortable(table) {
 		var th = table.tHead, i;
 		th && (th = th.rows[0]) && (th = th.cells);
@@ -167,11 +165,13 @@ console.log("-------------------------------");
 			th[i].addEventListener('click', function () {sortTable(table, i, (dir = 1 - dir))});
 			}(i));
 		}
+	// 	------------------------------
 	function makeAllSortable(parent) {
 		parent = parent || document.body;
 		var t = parent.getElementsByTagName('table'), i = t.length;
 		while (--i >= 0) makeSortable(t[i]);
 		}
+	// 	------------------------------
 	makeAllSortable();
 	// 	------------------------------
 	});
