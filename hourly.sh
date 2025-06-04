0055255f05f8bbd7512a3d2dcf6e0bd0f0ac60d8
David Craig Cothren <dacracot@yahoo.com>
2025-06-04 08:20:58 -0700

    oops, index became birding

--- hourly.sh
+++ hourly.sh
@@ -41,7 +41,7 @@ sleep ${HOWLONG}m
 # stop recording
 kill `cat ${WORK_HOUR}/recording.pid`
 rm ${WORK_HOUR}/recording.pid
-# # record weather data
+# record weather data
 MAXTRYS=5
 for i in $(seq 1 $MAXTRYS)
 do
@@ -84,7 +84,7 @@ gzip ${WORK_HOUR}/*
 # extract the table to XML
 sqlite3 ${BARCHART_HOME}/birds.db < ${BARCHART_HOME}/web/birding.sql > ${BARCHART_HOME}/web/birding.xml
 # transform the xml into html
-XSLTransform -s:${BARCHART_HOME}/web/birding.xml -xsl:${BARCHART_HOME}/web/birding.xsl > ${BARCHART_HOME}/web/index.html lat=${LAT} lon=${LON} asOf="${AS_OF}"
+XSLTransform -s:${BARCHART_HOME}/web/birding.xml -xsl:${BARCHART_HOME}/web/birding.xsl > ${BARCHART_HOME}/web/birding.html lat=${LAT} lon=${LON} asOf="${AS_OF}"
 # copy it all to the web server
 cp -v ${BARCHART_HOME}/web/favicon.ico ${WEB_HOME}/BirdNET-BarChart
 cp -v -R ${BARCHART_HOME}/web/grfx ${WEB_HOME}/BirdNET-BarChart
