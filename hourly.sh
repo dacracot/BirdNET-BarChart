046753a46d9ce01b1b5d30f67755fc71923c7b9f
David Craig Cothren <dacracot@yahoo.com>
2025-06-04 11:22:48 -0700

    locale with quotes

--- hourly.sh
+++ hourly.sh
@@ -84,7 +84,7 @@ gzip ${WORK_HOUR}/*
 # extract the table to XML
 sqlite3 ${BARCHART_HOME}/birds.db < ${BARCHART_HOME}/web/birding.sql > ${BARCHART_HOME}/web/birding.xml
 # transform the xml into html
-XSLTransform -s:${BARCHART_HOME}/web/birding.xml -xsl:${BARCHART_HOME}/web/birding.xsl > ${BARCHART_HOME}/web/birding.html locale=${LOCALE} asOf="${AS_OF}"
+XSLTransform -s:${BARCHART_HOME}/web/birding.xml -xsl:${BARCHART_HOME}/web/birding.xsl > ${BARCHART_HOME}/web/birding.html locale="${LOCALE}" asOf="${AS_OF}"
 # copy it all to the web server
 cp -v ${BARCHART_HOME}/web/favicon.ico ${WEB_HOME}/BirdNET-BarChart
 cp -v -R ${BARCHART_HOME}/web/grfx ${WEB_HOME}/BirdNET-BarChart
