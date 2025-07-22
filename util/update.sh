9c9a2cc6faef4144edfb03970d13cab166bb94e8
David Craig Cothren <dacracot@yahoo.com>
2025-07-21 18:02:17 -0700

    simple update script to pull main and post process if necessary, for instance SQL updates

---
+++ util/update.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+SECONDS=0
+# ---------------------------------------------------
+YEAR=`date '+%Y'`
+MONTH=`date '+%m'`
+DAY=`date '+%d'`
+# ---------------------------------------------------
+# source the configuration file
+# it must be edited and copied as ".BirdNET-BarChart" to you home directory
+CONFIG_FILE=${HOME}/.BirdNET-BarChart
+if [ -f "${CONFIG_FILE}" ]; then
+	source ${CONFIG_FILE}
+else 
+	echo " "
+	echo "${CONFIG_FILE} does not exist."
+	echo "Run the config.sh script to create."
+	echo " "
+	exit 1
+fi
+# ---------------------------------------------------
+{
+# go to installed root
+cd ${BARCHART_HOME}
+# checkout main
+git checkout main
+# pull latest
+git pull
+# scripting to adjust volatile components
+echo "no scripting necessary"
+# how long did it take
+DURATION=$SECONDS
+echo "$(($DURATION / 60)) minutes and $(($DURATION % 60)) seconds elapsed."
+}  >> ${BARCHART_HOME}/logs/${YEAR}-${MONTH}-${DAY}-weekly.out 2>> ${BARCHART_HOME}/logs/${YEAR}-${MONTH}-${DAY}-weekly.err
+# ---------------------------------------------------
