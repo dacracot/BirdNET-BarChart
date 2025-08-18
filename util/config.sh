d86083cdb6e7b17e9a9207c86aa87719ec0132d5
David Craig Cothren <dacracot@yahoo.com>
2025-08-18 10:05:32 -0700

    try this again

--- util/config.sh
+++ util/config.sh
@@ -7,22 +7,22 @@ MONTH=`date '+%m'`
 DAY=`date '+%d'`
 # ---------------------------------------------------
 # ---------------------------------------------------
-echo "You will be prompted for your Open Weather Map access token and BirdWeather access token." > /dev/tty
-echo "The Open Weather Map token is for recording and display current climate conditions.  You may obtain it from https://openweathermap.org. Without it your display will lack climate data." > /dev/tty
-echo "The BirdWeather token is for sharing data with the Birdweather maps.  You may obtain it from https://www.birdweather.com. This only necessary if you choose to share with Birdweather." > /dev/tty
-read -p "Do you wish to continue (y/n)?" YORN > /dev/tty
+echo "You will be prompted for your Open Weather Map access token and BirdWeather access token."
+echo "The Open Weather Map token is for recording and display current climate conditions.  You may obtain it from https://openweathermap.org. Without it your display will lack climate data."
+echo "The BirdWeather token is for sharing data with the Birdweather maps.  You may obtain it from https://www.birdweather.com. This only necessary if you choose to share with Birdweather."
+read -p "Do you wish to continue (y/n)?" YORN
 case "$YORN" in 
-  y|Y ) echo "yes" > /dev/tty;;
-  n|N ) echo "no" > /dev/tty; exit;;
-  * ) echo "Please answer Y or N" > /dev/tty; exit;;
+  y|Y ) echo "yes";;
+  n|N ) echo "no"; exit;;
+  * ) echo "Please answer Y or N"; exit;;
 esac
 # ---------------------------------------------------
 # does the CONFIG_FILE already exist?
 CONFIG_FILE=${HOME}/.BirdNET-BarChart
 if [ -f "${CONFIG_FILE}" ]; then
     source ${CONFIG_FILE}
-	echo "A configuration file at ${CONFIG_FILE} already exits." > /dev/tty
-	echo "Values from this file will be displayed as default choices." > /dev/tty
+	echo "A configuration file at ${CONFIG_FILE} already exits."
+	echo "Values from this file will be displayed as default choices."
 else 
 	LAT=45.0
 	LON=-100.0
@@ -39,68 +39,63 @@ else
 	FAILURE_EMAIL=username@somemail.com
 fi
 # ---------------------------------------------------
-# set BARCHART_HOME
-echo " " > /dev/tty
-read -e -p "Enter the home of the barchart software: " -i ${BARCHART_HOME} BARCHART_HOME > /dev/tty
-if [ -d "$BARCHART_HOME" ]; then
-	echo "Barchart home set to ${BARCHART_HOME}." > /dev/tty
-else
-	echo "${BARCHART_HOME} does not exist." > /dev/tty
-	exit
-fi
-{
 # set LAT LON
-echo " " > /dev/tty
-read -e -p "Enter your latitude: " -i ${LAT} LAT > /dev/tty
-echo "Latitude set to ${LAT}." > /dev/tty
-echo " " > /dev/tty
-read -e -p "Enter your longitude: " -i ${LON} LON > /dev/tty
-echo "Latitude set to ${LON}." > /dev/tty
+echo " "
+read -e -p "Enter your latitude: " -i ${LAT} LAT
+echo "Latitude set to ${LAT}."
+echo " "
+read -e -p "Enter your longitude: " -i ${LON} LON
+echo "Latitude set to ${LON}."
+# set BARCHART_HOME
+echo " "
+read -e -p "Enter the home of the barchart software: " -i ${BARCHART_HOME} BARCHART_HOME
+echo "Barchart home set to ${BARCHART_HOME}."
 # set ANALYZER_HOME... no initial default
-echo " " > /dev/tty
-read -e -p "Enter the home of the analyzer software: " -i ${ANALYZER_HOME} ANALYZER_HOME > /dev/tty
-echo "Analyzer home set to ${ANALYZER_HOME}." > /dev/tty
+echo " "
+read -e -p "Enter the home of the analyzer software: " -i ${ANALYZER_HOME} ANALYZER_HOME
+echo "Analyzer home set to ${ANALYZER_HOME}."
 # set XSLT_HOME... no initial default
-echo " " > /dev/tty
-read -e -p "Enter the home of the transformer software, specifically the directory containing the lib and bin subdirectories: " -i ${XSLT_HOME} XSLT_HOME > /dev/tty
-echo "Transformer home set to ${XSLT_HOME}." > /dev/tty
+echo " "
+read -e -p "Enter the home of the transformer software, specifically the directory containing the lib and bin subdirectories: " -i ${XSLT_HOME} XSLT_HOME
+echo "Transformer home set to ${XSLT_HOME}."
 # set WEB_HOME... /var/www/html/
-echo " " > /dev/tty
-read -e -p "Enter the home of the web server: " -i ${WEB_HOME} WEB_HOME > /dev/tty
-echo "Web server set to ${WEB_HOME}." > /dev/tty
+echo " "
+read -e -p "Enter the home of the web server: " -i ${WEB_HOME} WEB_HOME
+echo "Web server set to ${WEB_HOME}."
 # set PERCENT_STORAGE_ALLOWED... 60%
-echo " " > /dev/tty
-read -e -p "Enter the percentage of disk usage allowed: " -i ${PERCENT_STORAGE_ALLOWED} PERCENT_STORAGE_ALLOWED > /dev/tty
-echo "Percentage set to ${PERCENT_STORAGE_ALLOWED}." > /dev/tty
+echo " "
+read -e -p "Enter the percentage of disk usage allowed: " -i ${PERCENT_STORAGE_ALLOWED} PERCENT_STORAGE_ALLOWED
+echo "Percentage set to ${PERCENT_STORAGE_ALLOWED}."
 # set Open Weather Map Token... no initial default
-echo " " > /dev/tty
-read -e -p "Enter your Open Weather Map access token: " -i ${OWM_TOKEN} OWM_TOKEN > /dev/tty
-echo "Token set to ${OWM_TOKEN}." > /dev/tty
+echo " "
+read -e -p "Enter your Open Weather Map access token: " -i ${OWM_TOKEN} OWM_TOKEN
+echo "Token set to ${OWM_TOKEN}."
 # set BirdWeather Share Token... no initial default
-echo " " > /dev/tty
-read -e -p "Enter your BirdWeather access token [set to blank to disable]: " -i ${BIRDWEATHER_ID} BIRDWEATHER_ID > /dev/tty
-echo "Token set to ${BIRDWEATHER_ID}." > /dev/tty
+echo " "
+read -e -p "Enter your BirdWeather access token [set to blank to disable]: " -i ${BIRDWEATHER_ID} BIRDWEATHER_ID
+echo "Token set to ${BIRDWEATHER_ID}."
 # set BACKUP_HOME... default to read values
-echo " " > /dev/tty
-read -e -p "Enter the URI for the backup server [set to blank to disable]: " -i ${BACKUP_HOME} BACKUP_HOME > /dev/tty
-echo "Backup server set to ${BACKUP_HOME}." > /dev/tty
+echo " "
+read -e -p "Enter the URI for the backup server [set to blank to disable]: " -i ${BACKUP_HOME} BACKUP_HOME
+echo "Backup server set to ${BACKUP_HOME}."
 # set BACKUP_PASSWORD... no initial default
 if [ -z "$BACKUP_HOME" ]; then
     BACKUP_PASSWORD=""
 else
-	echo " " > /dev/tty
-	read -e -p "Enter the password for the backup server [set to blank to disable]: " -i ${BACKUP_PASSWORD} BACKUP_PASSWORD > /dev/tty
-	echo "Password set to ${BACKUP_PASSWORD}." > /dev/tty
+	echo " "
+	read -e -p "Enter the password for the backup server [set to blank to disable]: " -i ${BACKUP_PASSWORD} BACKUP_PASSWORD
+	echo "Password set to ${BACKUP_PASSWORD}."
 fi
 # set FAILURE_EMAIL... username@somemail.com
-echo " " > /dev/tty
-read -e -p "Enter the email for failure notifications: " -i ${FAILURE_EMAIL} NEW_FAILURE_EMAIL > /dev/tty
-echo "Email set to ${NEW_FAILURE_EMAIL}." > /dev/tty
-echo " " > /dev/tty
+echo " "
+read -e -p "Enter the email for failure notifications: " -i ${FAILURE_EMAIL} NEW_FAILURE_EMAIL
+echo "Email set to ${NEW_FAILURE_EMAIL}."
+echo " "
+{
 MAXTRYS=5
 for i in $(seq 1 $MAXTRYS)
 do
-	curl  --max-time 30 -H "Cache-Control: no-cache, no-store" "http://api.openweathermap.org/geo/1.0/reverse?lat=${LAT}&lon=${LON}&limit=1&appid=${OWM_TOKEN}" > ${BARCHART_HOME}/util/where.js
+	curl  --max-time 30 -H "Cache-Control: no-cache, no-store" "http://api.openweathermap.org/geo/1.0/reverse?lat=${LAT}&lon=${LON}&limit=1&appid=${OWM_TOKEN}xxx" > ${BARCHART_HOME}/util/where.js
 	EXITCODE=$?
 	if [[ $EXITCODE -eq 0 ]]
 		then
@@ -113,16 +108,17 @@ do
 	else
 		echo "curl: ${EXITCODE}"
 		cat ${BARCHART_HOME}/util/where.js
-		echo "===== locale FAILURE =====" > /dev/tty
+		echo "===== locale FAILURE ====="
 		sleep 5
 	fi
 	if [[ ${i} -eq ${MAXTRYS} ]]
 		then
-		echo "unable to set locale via lat/lon" > /dev/tty
+		echo "unable to set locale via lat/lon"
 		LOCALE="Somewhere, Unknown"
 	fi
 done
-echo "----------" > /dev/tty
+}  >> ${BARCHART_HOME}/logs/${YEAR}-${MONTH}-${DAY}-config.out 2>> ${BARCHART_HOME}/logs/${YEAR}-${MONTH}-${DAY}-config.err
+echo "----------"
 {
 echo "LAT=${LAT}"
 echo "LON=${LON}"
@@ -138,18 +134,18 @@ echo "OWM_TOKEN=${OWM_TOKEN}"
 echo "BIRDWEATHER_ID=${BIRDWEATHER_ID}"
 echo "FAILURE_EMAIL=${NEW_FAILURE_EMAIL}"
 }  > ${HOME}/.BirdNET-BarChart
-echo "Source set" > /dev/tty
+echo "Source set"
 # ---------------------------------------------------
-echo " " > /dev/tty
+echo " "
 if [ -f "${BARCHART_HOME}/birds.db" ]; then
-	echo "Do you wish to overwrite your birds database?" > /dev/tty
+	echo "Do you wish to overwrite your birds database?"
 	WARNON='\033[31;5m'
 	WARNOFF='\033[0m'
-	echo -e "${WARNON}" > /dev/tty
-	echo -e "--------------------------------------------------------" > /dev/tty
-	echo -e "YOU WILL LOSE ALL PREVIOUS DATA! THIS CAN NOT BE UNDONE!" > /dev/tty
-	echo -e "--------------------------------------------------------" > /dev/tty
-	echo -e "${WARNOFF}" > /dev/tty
+	echo -e "${WARNON}"
+	echo -e "--------------------------------------------------------"
+	echo -e "YOU WILL LOSE ALL PREVIOUS DATA! THIS CAN NOT BE UNDONE!"
+	echo -e "--------------------------------------------------------"
+	echo -e "${WARNOFF}"
 	select YN in "Yes" "No"; do
 		case $YN in
 			Yes ) sqlite3 ${BARCHART_HOME}/birds.db < ${BARCHART_HOME}/birds.db.ddl.sql;;
@@ -159,16 +155,14 @@ if [ -f "${BARCHART_HOME}/birds.db" ]; then
 else
 	sqlite3 ${BARCHART_HOME}/birds.db < ${BARCHART_HOME}/birds.db.ddl.sql
 fi
-echo "Database created" > /dev/tty
+echo "Database created"
 # ---------------------------------------------------
 echo " "
 sed -i "s/${FAILURE_EMAIL}/${NEW_FAILURE_EMAIL}/" ${BARCHART_HOME}/util/backupFailure.txt
 sed -i "s/${FAILURE_EMAIL}/${NEW_FAILURE_EMAIL}/" ${BARCHART_HOME}/util/storageFailure.txt
-echo "Email text updated" > /dev/tty
+echo "Email text updated"
 # ---------------------------------------------------
 # call crontab util
-echo "You may now start the analysis/processing." > /dev/tty
+echo "You may now start the analysis/processing."
 # ---------------------------------------------------
-echo "---------------------------------------------------------------------------------"
-}  >> ${BARCHART_HOME}/logs/${YEAR}-${MONTH}-${DAY}-config.out 2>> ${BARCHART_HOME}/logs/${YEAR}-${MONTH}-${DAY}-config.err
 
