84ea965203c0b54627e4a8f6bd1265c8b6b751b1
David Craig Cothren <dacracot@yahoo.com>
2025-09-02 10:29:54 -0700

    release 2.10.1

---
+++ util/updates/update-2.10.1-tasks.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+SECONDS=0
+# ---------------------------------------------------
+YEAR=`date '+%Y'`
+MONTH=`date '+%m'`
+DAY=`date '+%d'`
+# ---------------------------------------------------
+# source the configuration file
+# it must be edited and copied as ".BirdNET-BarChart" to you home directory
+CONFIG_FILE=${HOME}/.BirdNET-BarChart
+if [ -f "${CONFIG_FILE}" ]; then
+	source ${CONFIG_FILE}
+else 
+	echo " "
+	echo "${CONFIG_FILE} does not exist."
+	echo "Run the config.sh script to create."
+	echo " "
+	exit 1
+fi
+# ---------------------------------------------------
+{
+# check for semaphore
+if [ -f "${BARCHART_HOME}/util/updates/update-2.10.1-tasks.lock" ]; then
+	echo "previously run"
+else
+	touch "${BARCHART_HOME}/util/updates/update-2.10.1-tasks.lock"
+	# ===================================================================================
+	# ===================================================================================
+	#
+	# Update tasks coded here.
+	echo "no action necessary"
+	#
+	# ===================================================================================
+	# ===================================================================================
+fi
+# cascade the update to previous tasks
+${BARCHART_HOME}/util/updates/update-2.9-tasks.sh
+# how long did it take
+DURATION=$SECONDS
+echo "$(($DURATION / 60)) minutes and $(($DURATION % 60)) seconds elapsed."
+}  >> ${BARCHART_HOME}/logs/${YEAR}-${MONTH}-${DAY}-update-2.10.1-tasks.out 2>> ${BARCHART_HOME}/logs/${YEAR}-${MONTH}-${DAY}-update-2.10.1-tasks.err
+# ---------------------------------------------------
