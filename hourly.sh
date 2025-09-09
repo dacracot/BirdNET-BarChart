8a74c9ae80c57c49efc3887a86d3f9ddf68fe662
David Craig Cothren <dacracot@yahoo.com>
2025-09-08 20:25:35 -0700

    ignore snapshot files created and timestamped in hourly

--- hourly.sh
+++ hourly.sh
@@ -130,9 +130,12 @@ cp -v -R ${BARCHART_HOME}/web/grfx ${WEB_HOME}/BirdNET-BarChart
 cp -v ${BARCHART_HOME}/web/birding.* ${WEB_HOME}/BirdNET-BarChart
 cp -v ${BARCHART_HOME}/web/help.* ${WEB_HOME}/BirdNET-BarChart
 # snapshot the dial
-wkhtmltoimage --crop-w 306 --crop-h 296 --crop-x 12 --crop-y 210 http://localhost/BirdNET-BarChart/birding.html ${BARCHART_HOME}/web/grfx/lunar/snapshot-${DAY}-${HOUR}.png
+JULIAN=$(date '+%j')
+THIRTYDAYS=$(($JULIAN % 12))
+# this algorithm for 0..30 is certainly wrong for crossing into a new year
+wkhtmltoimage --crop-w 306 --crop-h 296 --crop-x 12 --crop-y 210 http://localhost/BirdNET-BarChart/birding.html ${BARCHART_HOME}/web/grfx/lunar/snapshot-${THIRTYDAYS}-${HOUR}.png
 TIMESTAMP=$(date '+%Y-%m-%d %H:00')
-convert -pointsize 18 -fill black -draw "text 2,294 '${TIMESTAMP}'" ${BARCHART_HOME}/web/grfx/lunar/snapshot-${DAY}-${HOUR}.png ${BARCHART_HOME}/web/grfx/lunar/snapshot-${DAY}-${HOUR}.png
+convert -pointsize 18 -fill black -draw "text 2,294 '${TIMESTAMP}'" ${BARCHART_HOME}/web/grfx/lunar/snapshot-${THIRTYDAYS}-${HOUR}.png ${BARCHART_HOME}/web/grfx/lunar/snapshot-${THIRTYDAYS}-${HOUR}.png
 # remove the warning semaphore
 rm -v "${BARCHART_HOME}/hourly.lock"
 # ===================================================
