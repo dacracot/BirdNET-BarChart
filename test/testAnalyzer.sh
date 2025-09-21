31e8b96e0829aa1b4e02088575e6e87c03e9b933
David Craig Cothren <dacracot@yahoo.com>
2025-09-21 13:06:37 -0700

    testing of installation structured

---
+++ test/testAnalyzer.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+SECONDS=0
+# ---------------------------------------------------
+YEAR=`date '+%Y'`
+MONTH=`date '+%m'`
+DAY=`date '+%d'`
+# ---------------------------------------------------
+# source the configuration file
+# it must be edited and copied as ".BirdNET-BarChart" to you home directory
+CONFIG_FILE=${HOME}/.BirdNET-BarChart
+if [ -f "${CONFIG_FILE}" ]; then
+    source ${CONFIG_FILE}
+else 
+	echo " "
+    echo "${CONFIG_FILE} does not exist."
+    echo "Run the config.sh script to create."
+	echo " "
+	exit 1
+fi
+# ---------------------------------------------------
+{
+# ===================================================
+pushd ${ANALYZER_HOME}
+source ${ANALYZER_HOME}/venv-birdnet/bin/activate
+# python3 -m birdnet_analyzer.analyze -o ${BARCHART_HOME}/test/ --rtype csv -t 3 ${BARCHART_HOME}/test/output.csv
+python3 -m birdnet_analyzer.analyze -o ${BARCHART_HOME}/test/output --rtype csv -t 3 ${BARCHART_HOME}/test/test.wav 
+popd
+diff ${BARCHART_HOME}/test/test.csv ${BARCHART_HOME}/test/output/test.BirdNET.results.csv
+RESULT=$?
+if [ $RESULT -eq 0 ]
+then
+	echo "test PASSED" > /dev/tty
+elif [ $RESULT -eq 1 ]
+then
+	{
+	echo "=================================="
+	echo "=================================="
+	echo "test FAILED"
+	echo "=================================="
+	echo "=================================="
+	} > /dev/tty
+else
+	{
+	echo "=================================="
+	echo "=================================="
+	echo "Something is wrong with the diff command."
+	echo "=================================="
+	echo "=================================="
+	} > /dev/tty
+fi
+# ===================================================
+# how long did it take
+DURATION=$SECONDS
+echo "$(($DURATION / 60)) minutes and $(($DURATION % 60)) seconds elapsed."
+echo "---------------------------------------------------------------------------------"
+}  >> ${BARCHART_HOME}/logs/${YEAR}-${MONTH}-${DAY}-testAnalyzer.out 2>> ${BARCHART_HOME}/logs/${YEAR}-${MONTH}-${DAY}-testAnalyzer.err
---
+++ test/testXSLT.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+SECONDS=0
+# ---------------------------------------------------
+YEAR=`date '+%Y'`
+MONTH=`date '+%m'`
+DAY=`date '+%d'`
+# ---------------------------------------------------
+# source the configuration file
+# it must be edited and copied as ".BirdNET-BarChart" to you home directory
+CONFIG_FILE=${HOME}/.BirdNET-BarChart
+if [ -f "${CONFIG_FILE}" ]; then
+    source ${CONFIG_FILE}
+else 
+	echo " "
+    echo "${CONFIG_FILE} does not exist."
+    echo "Run the config.sh script to create."
+	echo " "
+	exit 1
+fi
+# ---------------------------------------------------
+{
+# ===================================================
+java -classpath ${XSLT_HOME}/saxon-he-12.9.jar net.sf.saxon.Transform -s:${BARCHART_HOME}/test/test.xml -xsl:${BARCHART_HOME}/test/test.xsl > ${BARCHART_HOME}/test/output.html
+diff ${BARCHART_HOME}/test/test.html ${BARCHART_HOME}/test/output.html
+RESULT=$?
+if [ $RESULT -eq 0 ]
+then
+	echo "test PASSED" > /dev/tty
+elif [ $RESULT -eq 1 ]
+then
+	{
+	echo "=================================="
+	echo "=================================="
+	echo "test FAILED"
+	echo "=================================="
+	echo "=================================="
+	} > /dev/tty
+else
+	{
+	echo "=================================="
+	echo "=================================="
+	echo "Something is wrong with the diff command."
+	echo "=================================="
+	echo "=================================="
+	} > /dev/tty
+fi
+# ===================================================
+# how long did it take
+DURATION=$SECONDS
+echo "$(($DURATION / 60)) minutes and $(($DURATION % 60)) seconds elapsed."
+echo "---------------------------------------------------------------------------------"
+}  >> ${BARCHART_HOME}/logs/${YEAR}-${MONTH}-${DAY}-testXSLT.out 2>> ${BARCHART_HOME}/logs/${YEAR}-${MONTH}-${DAY}-testXSLT.err
